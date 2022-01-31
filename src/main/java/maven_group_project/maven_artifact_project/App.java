0d0f67343d3c5c32d485bc87fa77dc2c2f4d7f39
Luke Abougit <abougitluke@gmail.com>
2022-01-31 08:58:58 +0100

    project_maven

--- src/main/java/maven_group_project/maven_artifact_project/App.java
+++ src/main/java/maven_group_project/maven_artifact_project/App.java
@@ -5,8 +5,8 @@ package maven_group_project.maven_artifact_project;
  *
  */
 public class App 
-{
-    public static void main( String[] args )
+{ 
+    public static void main(  String[] args )
     {
         System.out.println( "Hello World!" );
     }
