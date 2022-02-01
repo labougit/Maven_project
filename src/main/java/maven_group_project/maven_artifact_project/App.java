88e7aa7cad7a8ec95798c3417e4c8004e7272755
labougit <98732552+labougit@users.noreply.github.com>
2022-02-01 16:03:31 +0100

    Update App.java

--- src/main/java/maven_group_project/maven_artifact_project/App.java
+++ src/main/java/maven_group_project/maven_artifact_project/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main(  String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Test réussi partie 5 branche "MAin2"" );
+
     }
 }
