08852a92362cd9b5be930e0b5de1b5dfc914ed91
labougit <98732552+labougit@users.noreply.github.com>
2022-02-01 16:14:54 +0100

    Update App.java

--- src/main/java/maven_group_project/maven_artifact_project/App.java
+++ src/main/java/maven_group_project/maven_artifact_project/App.java
@@ -8,6 +8,7 @@ public class App
 { 
     public static void main(  String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); //comentaire inutiles
+         System.out.println( "Test non réussis");
     }
 }
